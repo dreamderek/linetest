8e008488a11df351dc20d7809eb77bd1c107b1ab
dreamderek <dreamderek@gmail.com>
2020-12-10 16:39:23 +0800

    1222

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-let express = require('express');
 let linebot = require('linebot');
-
-let app = express();
-
-app.get('/', (res, req) =>{
-    req.send("test");
-});
-app.listen(8888);
-
-
 
 // 初始化 line bot 需要的資訊，在 Heroku 上的設定的 Config Vars，可參考 Step2
 let bot = linebot({
-  channelId: process.env.LINE_CHANNEL_ID,
-  channelSecret: process.env.LINE_CHANNEL_SECRET,
-  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
-
-
 
 // 當有人傳送訊息給 Bot 時
 bot.on('message', function (event) {
