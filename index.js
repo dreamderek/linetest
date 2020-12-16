475dc750a97aeb85fc311e3d22786909e95aa618
dreamderek <dreamderek@gmail.com>
2020-12-16 14:23:28 +0800

    123

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ let bot = linebot({
 // 當有人傳送訊息給 Bot 時
 bot.on('message', function (event) {
   // 回覆訊息給使用者 (一問一答所以是回覆不是推送)
+  event.reply(`你說了 ${event.message.text}`);
   let temp = "";
   event.forEach(element => {
     temp += element+"||";
   });
-  event.reply(`你說了 ${event.message.text}===${temp}`);
+  event.reply(`${temp}`);
 });
 
 // Bot 所監聽的 webhook 路徑與 port，heroku 會動態存取 port 所以不能用固定的 port，沒有的話用預設的 port 5000
